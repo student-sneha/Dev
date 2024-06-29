b4996d875eb5450167dc55f01e4ee7cc563f137c
Sneha <snehakhandelwal373@gmail.com>
2024-06-29 17:43:11 +0530

    new folder

---
+++ fruits/script.js
@@ -0,0 +1,18 @@
+// for(let i=0; i<=5; i++){
+//     console.log("Hello World",+i);
+// }
+
+// process & process.argv
+// let args= process.argv;
+// for(let i=2; i<=args.length; i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+// same directiory
+// const math = require("./Math");
+// console.log(math);
+
+// different directory
+const fruit = require("./fruits");
+console.log(fruit);
+
